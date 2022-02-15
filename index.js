84964995427d995fbdeb9b3eca78b5cd40d7db68
Anna Trofimenkova <annatrofimenkova@yandex.ru>
2022-02-15 21:01:22 +0300

    1

--- index.js
+++ index.js
@@ -8,15 +8,17 @@ const advance = document.getElementById('advance');
 const advanceTxt = document.querySelector('label[for=' + advance.value + ']').querySelector('.menu__inner-item-text').textContent;
 const profi = document.getElementById('profi');
 const profiTxt = document.querySelector('label[for=' + profi.value + ']').querySelector('.menu__inner-item-text').textContent;
+const buttons = document.querySelectorAll('.card-list__card-button');
+console.log(buttons);
 
 // Функция открытия-закрытия списков фильтров
 function closeOpenFilters(menuItem) {
-  if(menuItem.querySelector('.menu__item-icon')) {
-    menuItem.querySelector('.menu__item-icon').classList.toggle('menu__item-icon_active');
-  }
-  if(menuItem.querySelector('.menu__inner')) {
-    menuItem.querySelector('.menu__inner').classList.toggle('menu__inner_active');
-  }
+	if (menuItem.querySelector('.menu__item-icon')) {
+		menuItem.querySelector('.menu__item-icon').classList.toggle('menu__item-icon_active');
+	}
+	if (menuItem.querySelector('.menu__inner')) {
+		menuItem.querySelector('.menu__inner').classList.toggle('menu__inner_active');
+	}
 }
 // Функция создания тэгов
 function createTag(tagTxt, name) {
@@ -25,9 +27,9 @@ function createTag(tagTxt, name) {
 	const delButton = newTag;
 	newTag.querySelector('.tags__text').textContent = tagTxt;
 	// Кнопка удаления тэга
-	delButton.addEventListener('click',  () => {
-	  delButton.remove();
-    document.getElementById(name).checked = false;
+	delButton.addEventListener('click', () => {
+		delButton.remove();
+		document.getElementById(name).checked = false;
 	});
 	return newTag;
 }
@@ -37,28 +39,55 @@ function deleteTag(name) {
 }
 
 menuItems.forEach((item) => {
-  item.addEventListener('click',  evt => {
-    closeOpenFilters(item);
-  });
+	item.addEventListener('click', evt => {
+		closeOpenFilters(item);
+	});
 });
-novice.addEventListener('change', function() {
+novice.addEventListener('change', function () {
 	if (this.checked) {
 		tags.append(createTag(noviceTxt, this.id));
 	} else {
-    deleteTag(this.id);
-  }
+		deleteTag(this.id);
+	}
 });
-advance.addEventListener('change', function() {
+advance.addEventListener('change', function () {
 	if (this.checked) {
 		tags.append(createTag(advanceTxt, this.id));
 	} else {
-    deleteTag(this.id);
-  }
+		deleteTag(this.id);
+	}
 });
-profi.addEventListener('change', function() {
+profi.addEventListener('change', function () {
 	if (this.checked) {
 		tags.append(createTag(profiTxt, this.id));
 	} else {
-    deleteTag(this.id);
-  }
+		deleteTag(this.id);
+	}
 });
+
+
+// кнопка
+
+
+//button.addEventListener('click', function () {
+//if (button.classList.contains('card-list__card-button_continue')) {
+//console.log("кнопка продолжить")
+//}
+//else {
+//сonsole.log("другая кнопка")
+//}
+
+//});
+
+// ...
+buttons.forEach(button => {
+	button.addEventListener('click', function () {
+		if (button.classList.contains('card-list__card-button_sign-up')) {
+			button.classList.remove('card-list__card-button_sign-up');
+			button.classList.add('card-list__card-button_continue');
+			button.textContent = 'Продолжить';
+		}
+	})
+});
+
+
