3c747b7e88011c96e32887d3f8e3422784ac0082
Maksim Sokolov <vonrut@gmail.com>
2022-02-16 12:44:34 +0300

    кнопка очистки

--- index.js
+++ index.js
@@ -5,8 +5,10 @@ const tags = document.querySelector('.tags');
 const noviceTxt = document.querySelector('#novice-txt').textContent;
 const advanceTxt = document.querySelector('#advance-txt').textContent;
 const profiTxt = document.querySelector('#profi-txt').textContent;
+const eraseButton = document.querySelector('.menu__erase');
 const buttons = document.querySelectorAll('.card-list__card-button');
-//todo disabled; сброс таблицы;
+const activeCheckbox = document.querySelector('#active');
+const inactiveCheckbox = document.querySelector('#inactive');
 
 // Функция открытия-закрытия меню
 function closeOpenFilters(menuItem) {
@@ -30,39 +32,71 @@ function createTag(tagTxt, name) {
 	});
 	return newTag;
 }
+// Функция проверки на checked
+function check() {
+	let checked = false;
+	checkboxes = menuInnerItems;
+	for(var i=0, n=checkboxes.length; i<n; i++) {
+	  if (checkboxes[i].checked) checked = true;
+	}
+	return checked;
+}
 // Функция удаления тэгов
 function deleteTag(name) {
 	document.getElementsByName(name)[0].remove();
+}
+// Функция добавления слушателя для чекбоксов
+function addListener(item, name, text = 'noTag') {
+	item.querySelector(name).addEventListener('change', function() {
+		if (!check()){
+			eraseButton.classList.remove('menu__erase_active');
+		}
+		// Если нужно создавать тэг
+		if (this.checked && text !== 'noTag') {
+			tags.append(createTag(text, this.id));
+			eraseButton.classList.add('menu__erase_active');
+		} else if (this.checked && text === 'noTag') {
+			eraseButton.classList.add('menu__erase_active');
+			// для отключения взаимоисключающих статусов
+			if (name === '#inactive') {
+				activeCheckbox.disabled = true;
+			} else if (name === '#active') {
+				inactiveCheckbox.disabled = true;
+			}
+		} else if (!this.checked && text === 'noTag') {
+			if (name === '#inactive') {
+				activeCheckbox.disabled = false;
+			} else if (name === '#active') {
+				inactiveCheckbox.disabled = false;
+			}
+		} else if (text !== 'noTag') {
+			deleteTag(this.id);
+		}
+	});
 }
 
 // Слушатели чекбоксов
 menuInnerItems.forEach((item) => {
   if(item.querySelector('#novice')) {
-    item.querySelector('#novice').addEventListener('change', function() {
-      if (this.checked) {
-        tags.append(createTag(noviceTxt, this.id));
-      } else {
-        deleteTag(this.id);
-      }
-    });
+	addListener(item, '#novice', noviceTxt);
   }
   if(item.querySelector('#advance')) {
-    item.querySelector('#advance').addEventListener('change', function() {
-      if (this.checked) {
-        tags.append(createTag(advanceTxt, this.id));
-      } else {
-        deleteTag(this.id);
-      }
-    });
+	addListener(item, '#advance', advanceTxt);
   }
   if(item.querySelector('#profi')) {
-    item.querySelector('#profi').addEventListener('change', function() {
-      if (this.checked) {
-        tags.append(createTag(profiTxt, this.id));
-      } else {
-        deleteTag(this.id);
-      }
-    });
+	addListener(item, '#profi', profiTxt);
+  }
+  if(item.querySelector('#inactive')) {
+	addListener(item, '#inactive');
+  }
+  if(item.querySelector('#recorded')) {
+	addListener(item, '#recorded');
+  }
+  if(item.querySelector('#active')) {
+	addListener(item, '#active');
+  }
+  if(item.querySelector('#finished')) {
+	addListener(item, '#finished');
   }
 });
 
@@ -71,6 +105,15 @@ menuItems.forEach((item) => {
 	item.addEventListener('click', evt => {
 		closeOpenFilters(item);
 	});
+});
+// Слушатель кнопки очистки
+eraseButton.addEventListener('click', evt => {
+	tags.querySelectorAll('.tags__item').forEach(item => {
+		item.remove();
+	});
+	activeCheckbox.disabled = false;
+	inactiveCheckbox.disabled = false;
+	eraseButton.classList.remove('menu__erase_active');
 });
 
 // кнопка
