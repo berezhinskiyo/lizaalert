11a5532994bde967ead75c1465097b7e7c1376c4
ob <oberezhinskiy@gmail.com>
2022-02-17 14:21:03 +0300

    fix:  исправил js для ввода даты, отрефакторил скрипты

--- profile.js
+++
@@ -1,9 +0,0 @@
-const dateOfBirth = document.querySelector('#date-of-birth');
-
-//Создание маски ввода даты
-dateOfBirth.addEventListener('keyup', function (evt) {
-	var v = this.value;
-	if (v.match(/^\d{2}$/) !== null || v.match(/^\d{2}\.\d{2}$/) !== null) {
-		this.value = v + '.';
-	}
-});
